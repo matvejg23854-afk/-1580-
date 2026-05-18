3d9b9cd4c55a64e3818fce5a23f7175154e92f8c
Mathew <matvejg23854@gmail.com>
2026-05-18 22:34:09 +0300

    Update planet.py

--- planet.py
+++ planet.py
@@ -1,10 +1,31 @@
 class Planet:
     counter=0
     def __init__(self,name,radius,mass,distance,planet_type):
+        if not isinstance(name, str) or not name.strip():
+            raise ValueError("Название не может быть пустым")
+
+        if not isinstance(radius, (int, float)):
+            raise TypeError("Радиус должен быть числом")
+        if radius<=0:
+            raise ValueError("Радиус должен быть больше 0")
+
+        if not isinstance(mass, (int, float)):
+            raise TypeError("Масса должна быть числом")
+        if mass <= 0:
+            raise ValueError("Масса должна быть больше 0")
+
+        if not isinstance(distance, (int, float)):
+            raise TypeError("Расстояние между солнцем и планетой должно быть числом")
+        if distance <= 0:
+            raise ValueError("Расстояние между солнцем и планетой должно быть больше 0")
+
+        if not isinstance(planet_type, str) or not planet_type.strip():
+            raise ValueError("Тип планеты не может быть пустым")
+
         self.name=name
-        self.radius=radius
-        self.mass=mass
-        self.distance=distance
+        self.radius=float(radius)
+        self.mass=float(mass)
+        self.distance=float(distance)
         self.planet_type=planet_type
         Planet.counter+=1
         self.id=Planet.counter
